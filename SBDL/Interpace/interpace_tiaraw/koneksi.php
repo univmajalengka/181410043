cb7a856ae7ac05af30c1a6cf5f9016e4234c3b37
tiarawulandari <50090540+tiarawulandari@users.noreply.github.com>
2020-04-05 20:44:52 +0700

    Update koneksi.php

--- SBDL/Interpace/interpace_tiaraw/koneksi.php
+++ SBDL/Interpace/interpace_tiaraw/koneksi.php
@@ -3,6 +3,6 @@
 $host = mysql_connect("localhost","root","xxx");
 
 // isikan dengan nama database yang akan di hubungkan
-$db = mysql_select_db("malasngoding");
+$db = mysql_select_db("tiaraw");
 
 ?>
