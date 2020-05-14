499cc404b8a6465313046167e2a7f4fde3fa7d5d
tiarawulandari <50090540+tiarawulandari@users.noreply.github.com>
2020-05-14 22:33:02 +0700

    Delete pelanggan_delete.php

--- SBDL/Interface/pelanggan_delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "config.php";
-$id=$_POST['id'];
-$sqlup="delete from pelanggan where idPelanggan='$id'";
-$hapus=mysql_query($sqlup);
-?>
-<meta http-equiv="refresh" content="1;url=pelanggan_view.php">
